e9d91a40c0ef438583aad97335ac393b6394b539
klcatha <klcatha0518@gmail.com>
2018-11-12 14:07:32 +0900

    schecule detail update

--- app/src/main/java/com/example/kengomaruyama/myscheduler/ScheduleEditActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/myscheduler/ScheduleEditActivity.kt
@@ -2,6 +2,7 @@ package com.example.kengomaruyama.myscheduler
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateFormat
 import io.realm.Realm
 import io.realm.kotlin.createObject
 import io.realm.kotlin.where
@@ -21,20 +22,48 @@ class ScheduleEditActivity : AppCompatActivity() {
         setContentView(R.layout.activity_schedule_edit)
         realm = Realm.getDefaultInstance()
 
+        val scheduleId = intent?.getLongExtra("schedule_id", -1L)
+        if(scheduleId != -1L){
+            val schedule = realm.where<Schedule>()
+                    .equalTo("id",scheduleId).findFirst()
+            dateEdit.setText(
+                    DateFormat.format("yyyy/MM/dd", schedule?.date))
+            titleEdit.setText(schedule?.title)
+            detailEdit.setText(schedule?.detail)
+        }
+
         save.setOnClickListener {
-            realm.executeTransaction{
-                val maxId = realm.where<Schedule>().max("id")
-                val nextId = (maxId?.toLong() ?: 0L) + 1
-                val schedule = realm.createObject<Schedule>(nextId)
-                dateEdit.text.toString().toDate("yyyy/MM/dd")?.let{
-                    schedule.date = it
+            when (scheduleId) {
+                -1L ->{
+                    realm.executeTransaction{
+                        val maxId = realm.where<Schedule>().max("id")
+                        val nextId = (maxId?.toLong() ?: 0L) + 1
+                        val schedule = realm.createObject<Schedule>(nextId)
+                        dateEdit.text.toString().toDate("yyyy/MM/dd")?.let{
+                            schedule.date = it
+                        }
+                        schedule.title = titleEdit.text.toString()
+                        schedule.detail = detailEdit.text.toString()
+                    }
+                    alert("追加しました") {
+                        yesButton { finish() }
+                    }.show()
                 }
-                schedule.title = titleEdit.text.toString()
-                schedule.detail = detailEdit.text.toString()
+                else ->{
+                    realm.executeTransaction{
+                        val schedule = realm.where<Schedule>()
+                                .equalTo("id",scheduleId).findFirst()
+                        dateEdit.text.toString().toDate("yyyy/MM/dd")?.let {
+                            schedule?.date = it
+                        }
+                        schedule?.title = titleEdit.text.toString()
+                        schedule?.detail = detailEdit.text.toString()
+                    }
+                    alert("修正しました"){
+                        yesButton { finish() }
+                    }.show()
+                }
             }
-            alert("追加しました") {
-                yesButton { finish() }
-            }.show()
         }
     }
 
